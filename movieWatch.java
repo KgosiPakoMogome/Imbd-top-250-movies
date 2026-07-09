7f7b8231b71f7cb2bfea40f7b1a3c25645863bfc
Kgosi <202504897@ub.ac.bw>
2026-07-09 21:39:37 +0200

    Added random to generate a random number btwn 1 and 250

--- movieWatch.java
+++ movieWatch.java
@@ -1,2 +1,8 @@
-import random;
-public class MovieWatch {}
+import java.util.Random;
+public class movieWatch {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int movieNo = random.nextInt(1,250);
+        System.out.println(movieNo);
+    }
+}
